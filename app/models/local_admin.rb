e625745f953029f7a5419a7392186c8fff5fb32b
krupalimakwana <krupalimakwana18@gmail.com>
2016-04-27 13:27:02 +0530

    Devise integration

---
+++ app/models/local_admin.rb
@@ -0,0 +1,2 @@
+class LocalAdmin < User
+end
---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  def content_admin?
+  	self.type=="ContentAdmin" 
+  end
+  def local_admin?
+  	self.type=="LocalAdmin" 
+  end
+  def user?
+  	self.type=="User" 
+  end
+
+end
