d6557d330abed40486ac7b9db32f62bd3d008c69
krupalimakwana <krupalimakwana18@gmail.com>
2016-04-27 14:26:33 +0530

    change in association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ActiveRecord::Base
   has_many :assignments
+  has_many :pathways
+  belongs_to :department
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  #Helper method to check current_user is content_admin , local_admin or user
   def content_admin?
   	self.type=="ContentAdmin" 
   end
