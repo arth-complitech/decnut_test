d5916e5f38e851e7e108b498292e46931438f093
arth-complitech <arth@complitech.net>
2016-05-03 12:03:13 +0530

    Seed file commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+marriott = Group.create!(name: "Marriott")
+hyatt = Group.create!(name: "Hyatt")
+puts "=================+Groups Created============"
+
+marriott_newyork = Department.create!(name: "New York", group_id: marriott.id)
+marriott_los = Department.create!(name: "Los Angeles", group_id: marriott.id)
+
+hyatt_newyork = Department.create!(name: "New York", group_id: hyatt.id)
+hyatt_los = Department.create!(name: "Los Angeles", group_id: hyatt.id)
+puts "=====================+Departments Created=============="
+
+LocalUser.create!(email: "marriottlocaluser@yopmail.com", password: "12345678", password_confirmation: "12345678", first_name: "Local", last_name: "User", department_id: marriott_newyork.id, title: "Mr.", mobile_number: "9687047348")
+LocalAdmin.create!(email: "marriottlocaladmin@yopmail.com", password: "12345678", password_confirmation: "12345678", first_name: "Local", last_name: "User", department_id: marriott_newyork.id, title: "Mr.", mobile_number: "9687047348")
+ContentAdmin.create!(email: "marriottcontentadmin@yopmail.com", password: "12345678", password_confirmation: "12345678", first_name: "Local", last_name: "User", department_id: marriott_newyork.id, title: "Mr.", mobile_number: "9687047348")
+
+LocalUser.create!(email: "hyattlocaluser@yopmail.com", password: "12345678", password_confirmation: "12345678", first_name: "Local", last_name: "User", department_id: hyatt_newyork.id, title: "Mr.", mobile_number: "9687047348")
+LocalAdmin.create!(email: "hyattlocaladmin@yopmail.com", password: "12345678", password_confirmation: "12345678", first_name: "Local", last_name: "User", department_id: hyatt_newyork.id, title: "Mr.", mobile_number: "9687047348")
+ContentAdmin.create!(email: "hyattcontentadmin@yopmail.com", password: "12345678", password_confirmation: "12345678", first_name: "Local", last_name: "User", department_id: hyatt_newyork.id, title: "Mr.", mobile_number: "9687047348")
+puts "===========================+Users Created================"
