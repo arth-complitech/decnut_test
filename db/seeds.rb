f2485bc225bf89ac30626100eea1d6c1415ea9b3
pranav_trivedi <pranav@complitech.net>
2016-05-03 13:52:11 +0530

    change in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,10 +17,10 @@ hyatt_los = Department.create!(name: "Los Angeles", group_id: hyatt.id)
 puts "=====================+Departments Created=============="
 
 LocalUser.create!(email: "marriottlocaluser@yopmail.com", password: "12345678", password_confirmation: "12345678", first_name: "Local", last_name: "User", department_id: marriott_newyork.id, title: "Mr.", mobile_number: "9687047348")
-LocalAdmin.create!(email: "marriottlocaladmin@yopmail.com", password: "12345678", password_confirmation: "12345678", first_name: "Local", last_name: "User", department_id: marriott_newyork.id, title: "Mr.", mobile_number: "9687047348")
-ContentAdmin.create!(email: "marriottcontentadmin@yopmail.com", password: "12345678", password_confirmation: "12345678", first_name: "Local", last_name: "User", department_id: marriott_newyork.id, title: "Mr.", mobile_number: "9687047348")
+LocalAdmin.create!(email: "marriottlocaladmin@yopmail.com", password: "12345678", password_confirmation: "12345678", first_name: "Local", last_name: "Admin", department_id: marriott_newyork.id, title: "Mr.", mobile_number: "9687047348")
+ContentAdmin.create!(email: "marriottcontentadmin@yopmail.com", password: "12345678", password_confirmation: "12345678", first_name: "Content", last_name: "Admin", department_id: marriott_newyork.id, title: "Mr.", mobile_number: "9687047348")
 
 LocalUser.create!(email: "hyattlocaluser@yopmail.com", password: "12345678", password_confirmation: "12345678", first_name: "Local", last_name: "User", department_id: hyatt_newyork.id, title: "Mr.", mobile_number: "9687047348")
-LocalAdmin.create!(email: "hyattlocaladmin@yopmail.com", password: "12345678", password_confirmation: "12345678", first_name: "Local", last_name: "User", department_id: hyatt_newyork.id, title: "Mr.", mobile_number: "9687047348")
-ContentAdmin.create!(email: "hyattcontentadmin@yopmail.com", password: "12345678", password_confirmation: "12345678", first_name: "Local", last_name: "User", department_id: hyatt_newyork.id, title: "Mr.", mobile_number: "9687047348")
+LocalAdmin.create!(email: "hyattlocaladmin@yopmail.com", password: "12345678", password_confirmation: "12345678", first_name: "Local", last_name: "Admin", department_id: hyatt_newyork.id, title: "Mr.", mobile_number: "9687047348")
+ContentAdmin.create!(email: "hyattcontentadmin@yopmail.com", password: "12345678", password_confirmation: "12345678", first_name: "Content", last_name: "Admin", department_id: hyatt_newyork.id, title: "Mr.", mobile_number: "9687047348")
 puts "===========================+Users Created================"
