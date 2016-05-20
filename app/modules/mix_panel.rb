7990aef72cbe53e54a0d1af421f978805ba66289
krupalimakwana <krupali@complitech.net>
2016-05-20 15:33:11 +0530

    MixPanel merge fixes

--- app/modules/mix_panel.rb
+++ app/modules/mix_panel.rb
@@ -33,11 +33,24 @@ module MixPanel
 			    # '$company' => user.try(:company).try(:legal_business_name),
 	      })
 	  end	
-	 end
 
-  	def self.included(receiver)
-   	 receiver.extend         ClassMethods
-     receiver.send :include, InstanceMethods
-  	end
-	
+	  def mix_panel_sign_in(user,event)
+	  	tracker = Mixpanel::Tracker.new(Rails.application.secrets.mix_panel_token)
+
+			# Send an update to User1's profile
+      	tracker.track(user.email, event)
+
+      	tracker.people.set(user.email, {
+			    '$first_name' => user.first_name,
+			    '$last_name' => user.last_name,
+			    '$email' => user.email,
+			    # '$company' => user.try(:company).try(:legal_business_name),
+	      })
+	  end	
+	end
+
+  def self.included(receiver)
+    receiver.extend         ClassMethods
+    receiver.send :include, InstanceMethods
+  end
 end
