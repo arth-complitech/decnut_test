19bc4a6d660b3c81d947d8150c4cbc5d917835c1
krupalimakwana <krupali@complitech.net>
2016-05-20 15:51:18 +0530

    Mix Panel changes

--- app/modules/mix_panel.rb
+++ app/modules/mix_panel.rb
@@ -24,14 +24,14 @@ module MixPanel
 	  	tracker = Mixpanel::Tracker.new(Rails.application.secrets.mix_panel_token)
 
 			# Send an update to User1's profile
-      	tracker.track(user.email, event)
+      	# tracker.track(user.email, event)
 
-      	tracker.people.set(user.email, {
-			    '$first_name' => user.first_name,
-			    '$last_name' => user.last_name,
-			    '$email' => user.email,
-			    # '$company' => user.try(:company).try(:legal_business_name),
-	      })
+      	# tracker.people.set(user.email, {
+			    # '$first_name' => user.first_name,
+			    # '$last_name' => user.last_name,
+			    # '$email' => user.email,
+			    # # '$company' => user.try(:company).try(:legal_business_name),
+	      # })
 	  end	
 
 	  def mix_panel_sign_in(user,event)
