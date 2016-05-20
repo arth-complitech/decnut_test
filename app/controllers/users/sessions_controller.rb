c40b7edf74347b1bdc3a3f56f97f174a91ce6376
krupalimakwana <krupali@complitech.net>
2016-05-20 15:41:58 +0530

    Sessions Controller mix panel added

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,15 +2,16 @@ class Users::SessionsController < Devise::SessionsController
   include MixPanel
 # before_action :configure_sign_in_params, only: [:create]
   # GET /resource/sign_in
-  # def new
-  #   super
-  # end
-
-  # POST /resource/sign_in
-  def create
+  def new
     mix_panel_sign_in(current_user,"Sign In Page")
     super
   end
+
+  # POST /resource/sign_in
+  # def create
+  #   mix_panel_sign_in(current_user,"Sign In Page")
+  #   super
+  # end
 
   # DELETE /resource/sign_out
   # def destroy
