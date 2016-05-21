5b3145befadf822fe90bf216f1f20258d82a5d78
pranav_trivedi <pranav@complitech.net>
2016-05-21 16:53:22 +0530

    youtube solution

--- app/helpers/steps_helper.rb
+++ app/helpers/steps_helper.rb
@@ -1,6 +1,9 @@
 module StepsHelper
 	def embed(url_to_youtube)
-    youtube_id = url_to_youtube.try(:split,"=").last
-    content_tag(:iframe, nil, src: "//www.youtube.com/embed/#{youtube_id}")
+    youtube_id = url_to_youtube.try(:split,"=")
+    if youtube_id.present? 
+    	youtube_id = youtube_id.last
+		content_tag(:iframe, nil, src: "//www.youtube.com/embed/#{youtube_id}")
+  	end
   end
 end
