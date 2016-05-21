3c5fb74ec98a53363b0a4b4f101a934273503a85
pranav_trivedi <pranav@complitech.net>
2016-05-21 10:50:37 +0530

    spell check in text editor

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -1,4 +1,15 @@
 CKEDITOR.config.toolbar_mini = 
   [
-      ["Bold","Italic","Underline","-","NumberedList","BulletedList","-","Link","Unlink","-"]
+    ["Bold","Italic","Underline","-","NumberedList","BulletedList","-","Link","Unlink","-","Scayt"]
  ];
+// CKEDITOR.editorConfig = function (config) {
+//   // ... other configuration ...
+
+//   config.toolbar_mini = [
+//     ["Bold",  "Italic",  "Underline", "-",  "NumberedList", "BulletedList", "-","Link","Unlink","-","Scayt"],
+//   ];
+//   //config.toolbar = "simple";
+//   //config.disableNativeSpellChecker = true;
+//   config.scayt_autoStartup = true;
+//   // ... rest of the original config.js  ...
+// }
